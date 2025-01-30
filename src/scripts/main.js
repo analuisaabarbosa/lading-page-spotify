81960c6899b0844bcabdffb965b8aa5d8203f7e1
Ana Luisa <analuisaaugustob@gmail.com>
2025-01-30 14:23:18 -0300

    refactor: melhorando a função de carregamento dinâmico de componentes no main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,16 @@
-async function loadItem(id, src) {
+async function loadItem(id, src, append = false) {
     const container = document.getElementById(id);
     const response = await fetch(src);
     const content = await response.text();
-    container.innerHTML = content;
+    
+    if (append) {
+        container.insertAdjacentHTML('beforeend', content);
+    } else {
+        container.innerHTML = content;
+    }
 }
 
 loadItem('sidebar', './src/components/sidebar.html');
 loadItem('footer', './src/components/footer.html');
-loadItem('main-container', './src/components/headernav.html')
+loadItem('main-container', './src/components/headernav.html', true);
+loadItem('main-container', './src/sections/playlists-section.html', true);
