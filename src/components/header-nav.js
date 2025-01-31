dea70d4925b4910b381ac186f237abed98cd8132
Ana Luisa <analuisaaugustob@gmail.com>
2025-01-31 12:03:28 -0300

    feat: adicionando componentes 'Header', 'Footer', 'PlaylistsAndArtistsSection' e 'SidebarNav' para estrutura da página

---
+++ src/components/header-nav.js
@@ -0,0 +1,26 @@
+class HeaderNav extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <nav class="header__navigation">
+                <div class="navigation">
+                    <button class="arrow-left">
+                        <img src="./src/assets/icons/small-left.png" alt="Seta a esquerda">
+                    </button>
+                    <button class="arrow-right">
+                        <img src="./src/assets/icons/small-right.png" alt="Seta a direita">
+                    </button>
+                <div class="header__search">
+                    <img src="./src/assets/icons/search.svg" style="width: 16px; height: 16px; margin-left: 10px;" alt="Logo de lupa indicando a funcionalidade de pesquisa na navegação">
+                    <input id="search-input" type="text" maxlength="200" placeholder="O que você quer ouvir?">
+                </div>
+                </div>
+                <div class="header__login">
+                    <button class="subscribe">Cadastre-se</button>
+                    <button class="login">Entrar</button>
+                </div>
+            </nav>
+        `;
+    }
+}
+
+customElements.define('header-nav', HeaderNav);
