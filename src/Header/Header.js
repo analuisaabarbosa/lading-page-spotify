157c18869d156f2ab394f698a913c1abdb08cfd4
Ana Luisa <analuisaaugustob@gmail.com>
2025-01-31 18:17:06 -0300

    feat: criando componente Header com barra de pesquisa

---
+++ src/Header/Header.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Header.css";
+import "../Generic/Reset.css";
+import "../Generic/Vars.css";
+import smallLeft from "../assets/icons/small-left.png";
+import smallRight from "../assets/icons/small-right.png";
+import searchInput from "../assets/icons/search.svg";
+
+const Header = () => {
+  return (
+    <nav className="header__navigation">
+      <div className="navigation">
+        <button className="arrow-left">
+          <img src={smallLeft} alt="Seta a esquerda"></img>
+        </button>
+        <button className="arrow-right">
+          <img src={smallRight} alt="Seta a direita"></img>
+        </button>
+        <div className="header__search">
+          <img
+            src={searchInput}
+            id="search-icon"
+            alt="Logo de lupa indicando a funcionalidade de pesquisa na navegação"
+          ></img>
+          <input
+            id="search-input"
+            type="text"
+            maxLength="200"
+            placeholder="O que você quer ouvir?"
+          ></input>
+        </div>
+      </div>
+      <div className="header__login">
+        <button className="subscribe">Cadastre-se</button>
+        <button className="login">Entrar</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
